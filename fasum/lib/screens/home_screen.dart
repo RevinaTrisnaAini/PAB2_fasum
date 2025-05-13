34518c0f4e619672b6f3a1bd2a577ef7e349e4ca
RevinaTrisnaAini <revinatrisnaaini_2226240102@mhs.mdp.ac.id>
2025-05-13 13:04:33 +0700

    pembaruan

--- fasum/lib/screens/home_screen.dart
+++ fasum/lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fasum/screens/sign_in_screen.dart';
@@ -11,32 +14,66 @@ class HomeScreen extends StatelessWidget {
         MaterialPageRoute(builder: (context) => const SignInScreen()));
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme
+          .of(context)
+          .scaffoldBackgroundColor,
       appBar: AppBar(
-        title: const Text('Home'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text(
+          'Fasum'
+          , style: TextStyle(
+          color: Colors.green[600],
+          fontWeight: FontWeight.bold,)
+          ,),
         actions: [
           IconButton(
+            onPressed: _showCategoryFlter,
+            icon: Icon(Icons.filter_list),
+            tooltip: 'filter kategori',
+          ),
+          IconButton(
             onPressed: () {
-              signOut(context);
+              signOut();
             },
-            icon: const Icon(Icons.logout),
+            icon: Icon(Icons.logout),
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Currently no posts'),
+      body:  (
+        onReferces
+
+      )
+        child : Card(
+        elevation: 1,
+      color: Theme.of(context).colorScheme.surfaceContainerLow,
+      shadowColor: Theme.of(context).colorScheme.shadow,
+      margin: EdgeInsets.all(10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => AddPostScreen()),
-          );
-        },
-        child: const Icon(Icons.add),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (imageBase64 != null)
+            ClipRect(
+              borderRadius BorderRadius.vertical(
+              top: Radius.circular(10),
+             ),
+             child: Hero(
+             tag: heroTag,
+             child: Image.memory(
+             base64Decode(imageBase64),
+             fit: BoxFit.cover,
+             width: double.infinity,
+    height: 200,
+            Padding()
+    ),
+             )
+            )
+        ],
       ),
+    )
     );
-  }
-}
